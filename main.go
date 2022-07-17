9494697cbd7e7dd55df4a4f3cd0dd69fbc0b02eb
syafrin-ibrahim <syafrinibrahim12@gmail.com>
2022-07-17 13:04:39 +0800

    make handler api

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/lib/pq"
+	"github.com/syafrin-ibrahim/mybank/api"
+	db "github.com/syafrin-ibrahim/mybank/db/sqlc"
+	"github.com/syafrin-ibrahim/mybank/util"
+)
+
+func main() {
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config: ", err)
+	}
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
+	if err != nil {
+		log.Fatal("cannot connect to database", err)
+	}
+	store := db.NewStore(conn)
+	server := api.NewServer(store)
+	err = server.Start(config.ServerAddress)
+
+	if err != nil {
+		log.Fatal("cannot start server: ", err)
+	}
+}
